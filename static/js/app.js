3e4892a69b963ed4e4184b86189c616e7abaead6
Gabriel Pivaro <gabrconc@gmail.com>
2020-11-04 01:05:07 -0600

    Part I working

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,7 @@ for date/time, city, state, country, shape, and comment at the very least.*/
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
+var tbody_old = tbody
 
 // Step 1: Loop Through `data` and log each ufo observation object
 data.forEach(ufo => {
@@ -28,13 +29,51 @@ data.forEach(ufo => {
   });
 });
 
+
+///////////////////////////////////////////////////////////////////////////////////
+/* Use a date form in your HTML document and write JavaScript code that will listen 
+for events and search through the date/time column to find rows that match user input.
+*/
 ///////////////////////////////////////////////////////////////////////////////////
 
 // Assign the data from `data.js` to a descriptive variable
 var ufos = data;
 
-// Getting a reference to the input element on the page with the id property set to 'input-field'
+// Getting a reference to the input element on the page with the id property set to 'datetime'
 var inputField = d3.select("#datetime");
+
+// Complete the form handler for the form
+inputField.on("change", function () {
+
+  // Remove all table rows from tbody
+  var table = d3.select("tbody").selectAll("tr").remove()
+
+  var newText = d3.event.target.value;
+  console.log(newText);
+
+  var filteredUfos = ufos.filter(ufo => ufo.datetime === newText);
+  console.log(filteredUfos)
+
+  filteredUfos.forEach(ufo => {
+    //console.log(ufo);
+
+    // Step 2:  Use d3 to append one table row `tr` for each ufo observation object
+    var row = tbody.append("tr");
+
+    // Step 3:  Use `Object.entries` to log each ufo observation value
+    Object.entries(ufo).forEach(([key, value]) => {
+      //console.log(key, value);
+
+      // Step 4: Use d3 to append 1 cell per ufo observation value (Date, City, State, Country, Shape, Duration, Comments)
+      var cell = row.append("td");
+
+      // Step 5: Use d3 to update each cell's text with
+      // ufo observation values (Date, City, State, Country, Shape, Duration, Comments)
+      cell.text(value);
+    });
+  });
+
+});
 
 /*
 // Input fields can trigger a change event when new text is entered.
@@ -45,22 +84,19 @@ inputField.on("change", function() {
 */
 
 
-// Complete the form handler for the form
-inputField.on("change", function() {
-	
-
-  // Select the input element and get the raw HTML node
-  var newText = d3.event.target.value;
-  console.log(newText);
-
-  // Get the value property of the input element
-
-  // Use the form input to filter the data by blood type
-  var dateSelected = ufos.filter(ufo => {ufo.datetime === newText});
-  
-  console.log(dateSelected)
+// // Complete the form handler for the form
+// inputField.on("change", function () {
 
 
+//   // Select the input element and get the raw HTML node
+//   var newText = d3.event.target.value;
+//   console.log(newText);
 
-  
-});
+//   // Get the value property of the input element
+
+//   // Use the form input to filter the data by blood type
+//   var dateSelected = ufos.filter(ufo => { ufo.datetime === newText });
+
+//   console.log(dateSelected)
+
+// });
